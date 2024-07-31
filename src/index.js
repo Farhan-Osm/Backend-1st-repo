a203ed94a4ac603b1cf9c7c763acafd9311d821a
Farhan <farhanosm121@gmail.com>
2024-07-31 13:49:43 +0530

    utills setup

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// require('dotenv').config({path: './env'})
+
+// import mongoose from 'mongoose';
+// import DB_NAME from './constants'
+
+import connectDB from './db/index.js';
+import dotenv from "dotenv";
+
+dotenv.config({path: './env'})
+
+const port = process.env.PORT || 3000;
+connectDB()
+
+.then(() => {
+    app.listen(port,()  => {
+        console.log(`server is running at port ${port}`)
+    })
+
+    app.on("error", (err) => {
+        console.log("server error", err)
+    });
+})
+.catch((error) =>{
+    console.log("Mongo DB connection Failed!!", error);
+})
+
+
+
+
+
+
+
+
+// import express from 'express';
+
+// const app = express();
+
+
+// (async () =>{
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       app.on("error", (error) => {
+//         console.log("ERROR!", error);
+//         throw error;
+//       })
+
+// app.listen( process.env.PORT, () => {
+//     console.log(`app listening on port: ${process.env.PORT}`)
+// });
+
+//     } catch (error) {
+//         console.log("ERROR!",error);
+//         throw error;
+//     }
+// }) ();
+
---
+++ src/utils/asyncHendler.js
@@ -0,0 +1,29 @@
+import { Promise } from "mongoose";
+
+const asyncHendler = (requestHendler) => {
+    (req, res, next) => {
+        Promise.resolve (requestHendler(req, res, next))
+        .catch((err) => next(err));
+    };
+}
+
+export {asyncHendler} 
+
+
+
+
+// const asyncHendler = () => {};
+// const asyncHendler = () => () => {};
+// const asyncHendler = (func) => async() => {};
+
+
+// const asyncHendler = (fn) => async(req, res, next) => { 
+//     try {
+        
+//     } catch (error) {
+//         res.status(err.code || 404).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+//  }

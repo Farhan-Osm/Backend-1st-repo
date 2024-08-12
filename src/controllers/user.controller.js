5add8690f973ff6d1fc70e1f0c054df30f21d515
Farhan <farhanosm121@gmail.com>
2024-08-12 22:42:38 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { User } from '../models/user.model.js';
 import {ApiError} from '../utils/ApiError.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
+import jwt from "jsonwebtoken";
 
 
 const genrateAccessAndRefreshTokens = async(userId) => {
@@ -94,9 +95,9 @@ const registerUser = asyncHandler(async (req, res) => {
             new ApiResponse(200, createUser, "user registered successfully")
         )
 
-}) 
+}); 
 
-//  log in user
+//  login user
 const loginUser =  asyncHandler(async (req, res) => {
     // request body -> data
     // username or email
@@ -110,7 +111,7 @@ const loginUser =  asyncHandler(async (req, res) => {
     const {email, username, password} = req.body
 
     // username or email
-    if (!email || !username) {
+    if (!email && !username) {
       throw new ApiError(400, "username ro email is required");
     }
 
@@ -149,10 +150,9 @@ const loginUser =  asyncHandler(async (req, res) => {
         },"User logged In Successfully"
       )
      )
-})
+});
 
-//  log out User
-
+//  logout User
 const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
@@ -168,7 +168,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
+    secure: true
   };
 
   return res.status(200)
@@ -177,5 +177,50 @@ const logoutUser = asyncHandler(async (req, res) => {
   .json(new ApiResponse(200, {}, "user logout successfully"));
 });
 
+//  refresh Access Token
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incommingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-export { registerUser, loginUser, logoutUser }
+  if (!incommingRefreshToken){
+    throw new ApiError(401, "unauthorized request")
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incommingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+    )
+    const user = await User.findById(decodedToken?._id)
+  
+    if(!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+  
+    if (incommingRefreshToken !== user?.refreshToken){
+      throw new ApiError(401, "Refresh token is expired or invalid")
+    }
+  
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+  
+    const {accessToken, refreshToken} = await genrateAccessAndRefreshTokens(user._id)
+  
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {accessToken, refreshToken},
+        "Access Token refreshed"
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+  }
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
-import { User } from "../models/user.model";
-import { ApiError } from "../utils/ApiError";
-import asyncHendler from "../utils/asyncHendler";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import asyncHendler from "../utils/asyncHendler.js";
 import jwt from "jsonwebtoken";
 
 
